7baa13ef25795fbdeb5dd4b2979f3c9a91709fa8
Morozov Artem <mz.artemkr@gmail.com>
2025-11-02 19:20:06 +0300

    Added some basket features, little fixes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Модальное окно
+
+
 // Открытие модального окна
 
 document.getElementById("open-modal-button").addEventListener("click", function () {
@@ -30,7 +33,13 @@ document.getElementById("modal").addEventListener("click", event => {
 })
 
 
-// Скрипт для корзины
+
+
+
+
+
+
+// Корзина
 
 // Добавил прослушку на всё окно
 window.addEventListener("click", function (event) {
@@ -61,10 +70,66 @@ window.addEventListener("click", function (event) {
     if (event.target.dataset.action === "minus") {
 
         // Проверяем чтобы счётчик был больше 1
-        if(parseInt(counter.innerText) > 1) {
+        if (parseInt(counter.innerText) > 1) {
 
             // Уменьшаем счётчик на единицу
             counter.innerText = --counter.innerText
         }
+    }
+})
+
+
+// Обёртка в корзине, куда будут добавляться товары
+
+const orderWrapper = document.querySelector(".order-wrapper");
+
+
+// Отслеживаем клик на странице
+
+window.addEventListener("click", function (event) {
+
+    // Проверяем, что клик был совершён по кнопку "В корзину"
+    if (event.target.hasAttribute("data-in-basket")) {
+
+        // Находим карточку с товаром, внутри которой был совершён клик
+        const productItem = event.target.closest(".product-item")
+
+        // Собираем данные товара и записываем в объект
+
+        const productInfo = {
+            id: productItem.dataset.id,
+            imgSrc: productItem.querySelector(".product-img").getAttribute("src"),
+            title: productItem.querySelector(".product-name").innerText,
+            weight: productItem.querySelector(".product-weight").innerText,
+            calories: productItem.querySelector(".product-calories").innerText,
+            price: productItem.querySelector(".product-price").innerText,
+            counter: productItem.querySelector("[data-counter").innerText
+
+        }
+
+        // Собранные данные подставляем в шаблон в корзине
+
+
+        const orderItemHTML = `<div class="order-item">
+            <div class="order-item-description">
+            <div class="order-item-img">
+              <img src=${productInfo.imgSrc} width="40" alt="">
+            </div>
+            <div class="order-item-title">${productInfo.title}</div>
+            <div class="order-item-info">${productInfo.weight} / ${productInfo.calories}</div>
+            </div>
+            <div class="counter-and-price">
+            <div class="counter order-counter">
+              <div class="counter-control" data-action="minus">-</div>
+              <div class="counter-current" data-counter>${productInfo.counter}</div>
+              <div class="counter-control" data-action="plus">+</div>
+            </div>
+            <div class="order-irem-price">${productInfo.price}</div>
+            </div>
+          </div>
+          `
+
+        orderWrapper.insertAdjacentHTML("beforeend", orderItemHTML)
+
     }
 })
